fa2a48f051648e99e23fcfc3e45c8906315fd24b
Henrik Wenz <henrik.wenz@gmail.com>
2022-06-01 11:49:39 +0200

    [Example] Add Next.js 12 support
    #7

--- example/main/preload.js
+++ example/main/preload.js
@@ -1,4 +1,4 @@
-const { ipcRenderer, contextBridge } = require("electron");
+const { ipcRenderer, contextBridge, webFrame } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
   message: {
@@ -7,3 +7,18 @@ contextBridge.exposeInMainWorld("electron", {
     off: (handler) => ipcRenderer.off("message", handler),
   },
 });
+
+// Patch the global WebSocket constructor to use the correct DevServer url
+// More info: https://github.com/HaNdTriX/next-electron-server/issues/7
+webFrame.executeJavaScript(`Object.defineProperty(globalThis, 'WebSocket', {
+  value: new Proxy(WebSocket, {
+    construct: (Target, [url, protocols]) => {
+      if (url.endsWith('/_next/webpack-hmr')) {
+        // Fix the Next.js hmr client url
+        return new Target("ws://localhost:3000/_next/webpack-hmr", protocols)
+      } else {
+        return new Target(url, protocols)
+      }
+    }
+  })
+});`);
