32a3379e7b3bd849e507098852bbc9bb4397a998
Henrik Wenz <henrik.wenz@gmail.com>
2022-06-01 15:08:11 +0200

    [example] Improve 404 example

--- example/pages/404.js
+++ example/pages/404.js
@@ -1,3 +1,13 @@
+import OmniBox from "../components/OmniBox";
+import Navigation from "../components/Navigation";
+
 export default function NotFoundPage({ Component, pageProps }) {
-  return <h1>404 NotFound</h1>;
+  return (
+    <>
+      <h1>404 NotFound</h1>
+      <p>This is a custom 404 not found page!</p>
+      <OmniBox />
+      <Navigation />
+    </>
+  );
 }
